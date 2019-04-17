ad9381f3c7153e051c3fa3ff7bc92441e992f3bd
Touiyake <touiyake@gmail.com>
2019-04-17 17:13:15 +0800

    initial-commit

---
+++ src/main/java/com/primefaces/components/ThemeConverter.java
@@ -0,0 +1,38 @@
+package com.primefaces.components;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import org.primefaces.component.picklist.PickList;
+import org.primefaces.model.DualListModel;
+
+@FacesConverter(value = "themeConverter")
+public class ThemeConverter implements Converter {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object getAsObject(FacesContext fc, UIComponent ui, String value) {
+		DualListModel<Theme> model = (DualListModel<Theme>) ((PickList) ui).getValue();
+		for (Theme theme : model.getSource()) {
+			if (theme.getDisplayName().equalsIgnoreCase(value)) {
+				return theme;
+			}
+		}
+		
+		for (Theme theme : model.getTarget()) {
+			if (theme.getDisplayName().equalsIgnoreCase(value)) {
+				return theme;
+			}
+		}
+		
+		return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext fc, UIComponent ui, Object value) {
+		return ((Theme) value).getDisplayName();
+	}
+
+}
